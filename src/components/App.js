d26b27ffab5b088828cc6b831b1631b6a0f3cdd0
Anakole <104356239+Anakole@users.noreply.github.com>
2022-11-17 17:20:14 +0200

    add FriendList

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import user from './Profile/user.json';
 import Statistics from './Statistics/Statistics';
 import data from './Statistics/data.json';
 
+import FriendList from './FriendList/FriendList';
+import friends from './FriendList/friends.json';
+
 export const App = () => {
   return (
     <div>
@@ -16,8 +19,10 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
-      <Statistics title="Upload stats" stats={data} />
+      ;
+      <Statistics title="Upload stats" stats={data} />;
       <Statistics stats={data} />;
+      <FriendList friends={friends} />;
     </div>
   );
 };
