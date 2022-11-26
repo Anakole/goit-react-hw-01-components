1d470abf4d5c3199ed6274b60f2911e91793ee62
Anakole <104356239+Anakole@users.noreply.github.com>
2022-11-26 23:30:44 +0200

    Update TransactionHistory.jsx

--- src/components/Transaction/TransactionHistory.jsx
+++ src/components/Transaction/TransactionHistory.jsx
@@ -1,22 +1,27 @@
+import { TransactionTable } from './Transaction.styled';
+import { Section } from 'components/App/App.styled';
+
 export default function TransactionHistory({ items }) {
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+    <Section>
+      <TransactionTable>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </TransactionTable>
+    </Section>
   );
 }
