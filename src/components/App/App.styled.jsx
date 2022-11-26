24827bb84b450f054b7dc24007ab9c0c5fc1b0f6
Anakole <104356239+Anakole@users.noreply.github.com>
2022-11-26 23:31:36 +0200

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -6,3 +6,13 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const Section = styled.section`
+  width: 100%;
+  margin: auto;
+  background-color: #e7ecf2;
+  padding: 30px 0;
+  border-bottom: 2px solid #ffffff78;
+  display: flex;
+  justify-content: center;
+`;
