6739e47b296384e0017a65664044d07967c8fab5
Anakole <104356239+Anakole@users.noreply.github.com>
2022-11-26 23:30:50 +0200

    Create Transaction.styled.jsx

---
+++ src/components/Transaction/Transaction.styled.jsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const TransactionTable = styled.table`
+  background-color: white;
+  border-collapse: collapse;
+  text-align: center;
+  width: 800px;
+  color: rgb(133, 133, 133);
+
+  td,
+  th {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+
+  tr:nth-child(even) {
+    background-color: #f3f6f9;
+  }
+
+  tr:hover {
+    background-color: #dce3e8;
+  }
+
+  th {
+    text-transform: uppercase;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: center;
+    background-color: #0497aa;
+    color: white;
+  }
+`;
