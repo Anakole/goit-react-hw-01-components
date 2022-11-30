cfcf76e21d6eaf66bdd9795af12f9c7a5594bf2a
Anakole <104356239+Anakole@users.noreply.github.com>
2022-11-30 22:56:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,30 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 // function toCamelCase(str) {
 //   const splitSrt = str.replace(/[^A-Z0-9]/gi, '_').split('_');
-//   console.log(splitSrt);
+
+//   let finArray = [];
+
+//   finArray.push(splitSrt.splice(0, 1));
+
+//   for (const word of splitSrt) {
+//     let wordArray = word.split('');
+//     const firstLetter = wordArray[0].toUpperCase();
+
+//     wordArray.splice(0, 1, firstLetter);
+//     const wordJoin = wordArray.join('');
+
+//     finArray.push(wordJoin);
+//   }
+
+//   const finSrting = finArray.join('');
+
+//   return finSrting;
 // }
 
-// toCamelCase('the-stealth-warrior');
+function toCamelCase(str) {
+  var regExp = /[-_]\w/gi;
+  return str.replace(regExp, function (match) {
+    return match.charAt(1).toUpperCase();
+  });
+}
+console.log(toCamelCase('The_stealth-warrior'));
